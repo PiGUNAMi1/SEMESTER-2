1bf7e7f0d39b696ec36e404158abfd9961400072
Kiran Saptaguna <152067550+PiGUNAMi1@users.noreply.github.com>
2024-01-15 09:37:21 +0700

    Add files via upload

---
+++ Kiran TKJ X-A/DDJ SEM 2/P1/perkenalan.py
@@ -0,0 +1,6 @@
+nama = "Kiran Saptaguna"
+jurusan = "TKJ"
+kelas = "10"
+
+print("halo perkenalkan saya", nama)
+print("Saya kelas", kelas, "dari jurusan", jurusan)
---
+++ Kiran TKJ X-A/DDJ SEM 2/P1/variable.py
@@ -0,0 +1,18 @@
+x = 7
+y = 8
+z = "SMK MEDIKACOM"
+p = False
+q = True
+
+print(x)
+print(y)
+print(z)
+print(p)
+print(q)
+
+print("cek tipe data")
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(p))
+print(type(q))
