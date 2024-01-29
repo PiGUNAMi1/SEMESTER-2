3a1c3211afb09c39f60e1b3929518e154b930d3e
Kiran Saptaguna <152067550+PiGUNAMi1@users.noreply.github.com>
2024-01-29 09:32:20 +0700

    Add files via upload

---
+++ p3/operator_penugasan.py
@@ -0,0 +1,19 @@
+x = 10
+
+x += 3
+print("penjumlahan",x)
+
+x -= 1
+print("pengurangan",x)
+
+x /= 1
+print("pembagian",x)
+
+x *= 1
+print("perkalian",x)
+
+x **= 1
+print("perpangkatan",x)
+
+x %= 1
+print("modulus",x)
